ad25648a09d81c58cb3aae1f97a4695ad97b4e8d
Yuvraj Jaiswal <quicksilver92571331@gmail.com>
2023-07-16 21:57:21 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,81 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let started = false;
+let level = 0;
+let score=0;
+const time=30000;
+setTimeout(function(){
+  window.location.href="thankyou.html";
+}, time);
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+  
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+  }
+
+  function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+  }
+
+  function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+  }
+
+  $(document).keypress(function() {
+    if (!started) {
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
+
+$(".btn").click(function() {
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+          score+=5;
+        }, 1000);
+      }
+    } 
+    else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+      $("h1").after("<h2 id='level-title' class='score'>Total Score: " + score + "</h2>");
+      startOver();
+    }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+  score=0;
+}
+
+$("h1").text("Press A Key to Start");
